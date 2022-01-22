4d492eb4522a5ae742306b6243d6506ce5d94b54
Al Bowles <akatch@users.noreply.github.com>
2022-01-22 12:59:19 -0600

    Appease shellcheck

--- .bin/backup.sh
+++ .bin/backup.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 set -e
 
-DATE=`date +%F`
+DATE="$(date +%F)"
 SOURCES="$HOME/pictures $HOME/code $HOME/documents $HOME/ebooks/archive /etc/fstab"
-RSYNCOPTS="-rzxl --delete --delete-excluded --exclude-from=$HOME/.config/rsync/exclude"
 DEST="$HOME/backups"
 
-mkdir -p $DEST/$HOSTNAME
-cd $DEST/$HOSTNAME
+mkdir -p "$DEST/$HOSTNAME"
+cd "$DEST/$HOSTNAME"
 
 # TODO encrypt tarball
 # TODO find a way to not output 'removing leading / from [...]'
-tar --create --file $USER@$HOSTNAME-$DATE.tar.zst --use-compress-program=zstd $SOURCES
+tar --create --file "$USER@$HOSTNAME-$DATE.tar.zst" --use-compress-program=zstd "$SOURCES"
 find ./ -mindepth 1 -maxdepth 1 -ctime +2 -exec rm -rf '{}' \;
