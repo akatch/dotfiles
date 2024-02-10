0c4e8a4187fbd2a787c592de223bcc259048e4c7
Al Bowles <bowlesalx@gmail.com>
2024-02-10 17:46:24 -0600

    feat: ship to samba

--- .bin/backup.sh
+++ .bin/backup.sh
@@ -4,6 +4,7 @@ set -e
 DATE="$(date +%F)"
 SOURCES="$HOME/pictures $HOME/code $HOME/documents $HOME/ebooks/archive /etc/fstab"
 DEST="$HOME/backups"
+OFFSITE_DEST="/vault/al"
 
 mkdir -p "$DEST/$HOSTNAME"
 cd "$DEST/$HOSTNAME"
@@ -19,3 +20,5 @@ tar --create \
     --exclude='*.qcow2' \
     --exclude='*.img' $SOURCES
 find ./ -mindepth 1 -maxdepth 1 -ctime +1 -exec rm -rf '{}' \;
+
+rsync -az --delete ./ "$OFFSITE_DEST/"
